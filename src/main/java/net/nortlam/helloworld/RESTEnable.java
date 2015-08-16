31c36fd77de65d49ed6378eec97218923b19021d
Mauricio Leal <maltron@gmail.com>
2015-08-16 20:14:52 -0300

    First Commit

---
+++ src/main/java/net/nortlam/helloworld/RESTEnable.java
@@ -0,0 +1,9 @@
+package net.nortlam.helloworld;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class RESTEnable extends Application {
+    
+}
---
+++ src/main/java/net/nortlam/helloworld/Resource.java
@@ -0,0 +1,16 @@
+package net.nortlam.helloworld;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/resource")
+public class Resource {
+    
+    @GET @Produces(MediaType.TEXT_PLAIN)
+    public String myName() {
+        return "Mauricio Leal";
+    }
+    
+}
